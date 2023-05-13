65ee4a45dca6e41d5c5dcf7899ecf90c0b98d3ab
amineennaciri <spitlight.enn@gmail.com>
2023-05-13 16:25:13 +0100

    created the html files and made the index.js a server able to respond to my requests with the right files. added a 404 page in case of a request that my server can't response.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # odin-node-basic-info-site
+This is the basic informational site project from TOP(the odin project) Full Stack JavaScript course.
+
+This project is about creating a very basic informational site that contains 4 pages: indes, about, contact-me and 404. The purpose is to practice Node.js principles.
+
+By the end of the project, I will have demonstrated skills in using Node.js
+
+Self-reflection and conclusion: this was an opportunity to practice creating a server from scratch hosted on my machine using Node.js
---
+++ index.js
@@ -0,0 +1,53 @@
+// declaring modules.
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+
+//
+const server = http.createServer((req, res) => {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/about') {
+    fs.readFile('about.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/contact-me') {
+    fs.readFile('contact-me.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else{
+    fs.readFile('404.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  /* else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  } */
+});
+
+server.listen(8080);
